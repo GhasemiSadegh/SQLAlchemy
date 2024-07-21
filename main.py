78a4dd37ad950311c92a7597e65a0ff1dbee3760
MrNoobPy <tarjomebama@gmail.com>
2024-07-21 09:14:53 +0200

    SQLAlchemy: Installing Plug in: DataBase Navigator

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
 db_url = "sqlite:///mydata.db"
 engine = create_engine(db_url)
+Base.metadata.create_all(engine)
