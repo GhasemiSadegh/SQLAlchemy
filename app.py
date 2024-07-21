367177e6ecd609d55152712d2b8d72e845f7f927
MrNoobPy <tarjomebama@gmail.com>
2024-07-21 10:42:27 +0200

    SQLAlchemy: Adding multiple users and printing the given id, name, age

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 user = User(name='Sadegh', age=30)
-
+user1 = User(name='Reza', age=40)
+user2 = User(name='Ali', age=15)
 session.add(user)
 
+
+session.add_all([user1, user2])
 session.commit()
+
+# to print users
+users = session.query(User).all()
+print(users)
+
+# accessing user's parameters
+
+user = users[0]
+print(user.id,
+      user.name,
+      user.age)
