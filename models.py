26e70bb4f75509a0ef917d87f2708846779c99ea
MrNoobPy <tarjomebama@gmail.com>
2024-07-24 13:53:53 +0200

    SQLAlchemy: Practicing filter more

--- models.py
+++ models.py
@@ -1,8 +1,5 @@
-from sqlalchemy import Column, String, Integer, create_engine, or_, and_, not_
-
-
+from sqlalchemy import Column, String, Integer, create_engine, not_
 from sqlalchemy.orm import declarative_base, sessionmaker
-
 
 Base = declarative_base()
 db_url = 'sqlite:///db.sqlite'
@@ -18,7 +15,6 @@ class User(Base):
 
 Base.metadata.create_all(engine)
 
-
 Session = sessionmaker(bind=engine)
 session = Session()
 session.commit()
@@ -30,9 +26,9 @@ session.commit()
 #
 # session.add_all([user1, user2, user3, user4])
 # session.commit()
-# session.close()
 
+users = session.query(User).where(User.age >= 34)
 
-users = session.query(User).filter_by(age=34)
 for user in users:
     print(user.name, user.age)
+
