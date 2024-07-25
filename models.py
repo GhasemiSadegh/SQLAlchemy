2c0c2adc9f273f692f748653872ff45836393dc3
MrNoobPy <tarjomebama@gmail.com>
2024-07-25 22:26:59 +0200

    SQLAlchemy: Relationships, creating first class

--- models.py
+++ models.py
@@ -9,3 +9,18 @@ session = Session()
 Base = declarative_base()
 
 
+class BaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
+    id = Column(Integer, ForeignKey=True)
+
+
+class Addresses(BaseModel):
+    __tablename__ = "addresses"
+
+    city = Column(String)
+    state = Column(String)
+    zip_code = Column(Integer)
+
+
+class User(BaseModel):
