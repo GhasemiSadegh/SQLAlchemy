3248e88abb54f1e1add66b5d245b9c45bdfb8614
MrNoobPy <tarjomebama@gmail.com>
2024-07-25 22:13:26 +0200

    SQLAlchemy: Relationships(non-mapped method)

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import (Column, ForeignKey, String, Integer, create_engine)
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
+db_url = "sqlite:///database.db"
+engine = create_engine(db_url)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
